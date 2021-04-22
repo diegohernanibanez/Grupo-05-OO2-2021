5c792d7544bf03aa48a736b9017336ed0c847698
Diego Hernan <diego.hernan.ibanez@gmail.com>
2021-04-21 22:02:28 -0300

    Cambios de ejemplo

--- src/test/TestHBM.java
+++ src/test/TestHBM.java
@@ -7,6 +7,6 @@ public class TestHBM {
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
         session.close();
-        System.out.println("OK");
+        System.out.println("OK master");
     }
 }
