961e1507266b6abda5d222602813f8bbf5f67cbd
punisher-113 <punisher-1213@hotmail.com>
2021-05-31 19:16:53 -0300

    se agregan los buscar

--- src/main/java/com/example/aplication/controller/BuscarController.java
+++ src/main/java/com/example/aplication/controller/BuscarController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,6 +26,35 @@ public class BuscarController {
 
         return "views/buscar/buscarPersona";
     }
+
+
+    @RequestMapping("/buscar/permisoPerido")
+    public String buscarPermisoPeriodo(Model model, @Param("dni") Long dni) {
+        List<Persona> listProducts = personaServiceImplements.listarDni(dni);
+        model.addAttribute("listProducts", listProducts);
+        model.addAttribute("dni", dni);
+
+        return "views/buscar/buscarPermisoPeriodo";
+    }
+  
+    @RequestMapping("/buscar/permisoDiario")
+    public String buscarPermisoDiario(Model model, @Param("dni") Long dni) {
+        List<Persona> listProducts = personaServiceImplements.listarDni(dni);
+        model.addAttribute("listProducts", listProducts);
+        model.addAttribute("dni", dni);
+
+        return "views/buscar/buscarPermisoDiario";
+    }
+  
+    @RequestMapping("/buscar/permisoRodado")
+    public String buscarPermisoRodado(Model model, @Param("dni") Long dni) {
+        List<Persona> listProducts = personaServiceImplements.listarDni(dni);
+        model.addAttribute("listProducts", listProducts);
+        model.addAttribute("dni", dni);
+
+        return "views/buscar/buscarRodado";
+    }
+  
 
 
 
