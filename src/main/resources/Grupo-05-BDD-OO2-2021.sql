891e6f4db34d7bdea86effcf367ea123d092c620
punisher-113 <punisher-1213@hotmail.com>
2021-06-04 11:49:26 -0300

    se edita la bd final

--- src/main/resources/Grupo-05-BDD-OO2-2021.sql
+++ src/main/resources/Grupo-05-BDD-OO2-2021.sql
@@ -2,14 +2,13 @@
 --
 -- Host: localhost    Database: spring_db
 -- ------------------------------------------------------
--- Server version	8.0.17
+-- Server version	8.0.23
 
 -- ------ Inicialización de BD -------------
 drop database if exists spring_db;
 create database if not exists spring_db;
 use spring_db;
 -- ------------------------------------------
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,6 +22,191 @@ use spring_db;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `lugar`
+--
+
+DROP TABLE IF EXISTS `lugar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lugar` (
+  `id_lugar` bigint NOT NULL AUTO_INCREMENT,
+  `codigo_postal` varchar(8) NOT NULL,
+  `lugar` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_lugar`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lugar`
+--
+
+LOCK TABLES `lugar` WRITE;
+/*!40000 ALTER TABLE `lugar` DISABLE KEYS */;
+INSERT INTO `lugar` VALUES (1,'1834','MonteGrande'),(2,'1977','Lomas de zamora'),(3,'1999','Lanus'),(4,'2099','Las Toninas'),(5,'2100','Mar del tuyu');
+/*!40000 ALTER TABLE `lugar` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pemisos_lugares`
+--
+
+DROP TABLE IF EXISTS `pemisos_lugares`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pemisos_lugares` (
+  `fk_lugar` int NOT NULL,
+  `fk_permiso` bigint NOT NULL,
+  PRIMARY KEY (`fk_lugar`,`fk_permiso`),
+  KEY `FK5xtvd1sleq8j9ib7iv8sex22a` (`fk_permiso`),
+  CONSTRAINT `FK5xtvd1sleq8j9ib7iv8sex22a` FOREIGN KEY (`fk_permiso`) REFERENCES `lugar` (`id_lugar`),
+  CONSTRAINT `FKj9ae46shsntn6112cwugk8lf2` FOREIGN KEY (`fk_lugar`) REFERENCES `permiso` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pemisos_lugares`
+--
+
+LOCK TABLES `pemisos_lugares` WRITE;
+/*!40000 ALTER TABLE `pemisos_lugares` DISABLE KEYS */;
+INSERT INTO `pemisos_lugares` VALUES (1,1),(2,1),(1,5),(2,5);
+/*!40000 ALTER TABLE `pemisos_lugares` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permiso`
+--
+
+DROP TABLE IF EXISTS `permiso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `permiso` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fecha` date NOT NULL,
+  `id_persona` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK8woob4cm5meiecdgbmeej96am` (`id_persona`),
+  CONSTRAINT `FK8woob4cm5meiecdgbmeej96am` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permiso`
+--
+
+LOCK TABLES `permiso` WRITE;
+/*!40000 ALTER TABLE `permiso` DISABLE KEYS */;
+INSERT INTO `permiso` VALUES (1,'2021-06-04',1),(2,'2021-06-04',5);
+/*!40000 ALTER TABLE `permiso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permisodiario`
+--
+
+DROP TABLE IF EXISTS `permisodiario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `permisodiario` (
+  `motivo` varchar(45) NOT NULL,
+  `id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `FKnwt8k1cayvtinytftu3xs1gdw` FOREIGN KEY (`id`) REFERENCES `permiso` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permisodiario`
+--
+
+LOCK TABLES `permisodiario` WRITE;
+/*!40000 ALTER TABLE `permisodiario` DISABLE KEYS */;
+INSERT INTO `permisodiario` VALUES ('vacaciones',1),('trabajo',2);
+/*!40000 ALTER TABLE `permisodiario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permisoperiodo`
+--
+
+DROP TABLE IF EXISTS `permisoperiodo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `permisoperiodo` (
+  `cant_dias` int NOT NULL,
+  `vacaciones` tinyint(1) NOT NULL DEFAULT '0',
+  `id` int NOT NULL,
+  `fk_id_rodado` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKsgmliwygkdbrvyk87vfojcp4r` (`fk_id_rodado`),
+  CONSTRAINT `FKf0x3rqglvfna95kng7h0lj07` FOREIGN KEY (`id`) REFERENCES `permiso` (`id`),
+  CONSTRAINT `FKsgmliwygkdbrvyk87vfojcp4r` FOREIGN KEY (`fk_id_rodado`) REFERENCES `rodado` (`id_rodado`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permisoperiodo`
+--
+
+LOCK TABLES `permisoperiodo` WRITE;
+/*!40000 ALTER TABLE `permisoperiodo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permisoperiodo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `persona`
+--
+
+DROP TABLE IF EXISTS `persona`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `persona` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `apellido` varchar(45) NOT NULL,
+  `dni` bigint NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '1',
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_hlwyecu2r9wagqayhej1kt5wy` (`dni`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `persona`
+--
+
+LOCK TABLES `persona` WRITE;
+/*!40000 ALTER TABLE `persona` DISABLE KEYS */;
+INSERT INTO `persona` VALUES (1,'velez',38705112,1,'matias'),(2,'ibanez',38705100,1,'diego'),(3,'sassenus',40303030,1,'milagros'),(4,'ortiz',40303333,1,'aylen'),(5,'bermudez',41666666,1,'luz'),(6,'pere<',70602033,1,'juan'),(7,'lopez',10999999,1,'franco'),(8,'perez',10999777,1,'franco');
+/*!40000 ALTER TABLE `persona` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rodado`
+--
+
+DROP TABLE IF EXISTS `rodado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rodado` (
+  `id_rodado` int NOT NULL AUTO_INCREMENT,
+  `dominio` varchar(255) NOT NULL,
+  `vehiculo` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_rodado`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rodado`
+--
+
+LOCK TABLES `rodado` WRITE;
+/*!40000 ALTER TABLE `rodado` DISABLE KEYS */;
+INSERT INTO `rodado` VALUES (1,'bww999','Auto');
+/*!40000 ALTER TABLE `rodado` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `role`
 --
 
@@ -30,11 +214,11 @@ DROP TABLE IF EXISTS `role`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `role` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tipo` varchar(50) NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `enabled` bit(1) DEFAULT NULL,
+  `tipo` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +227,7 @@ CREATE TABLE `role` (
 
 LOCK TABLES `role` WRITE;
 /*!40000 ALTER TABLE `role` DISABLE KEYS */;
-INSERT INTO `role` VALUES (1,'ROLE_ADMIN',_binary ''),(2,'ROLE_AUDITOR',_binary '');
+INSERT INTO `role` VALUES (1,_binary '','ROLE_ADMIN'),(2,_binary '','ROLE_AUDITOR');
 /*!40000 ALTER TABLE `role` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,20 +239,17 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `apellido` varchar(45) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `tipo_documento` varchar(255) NOT NULL,
-  `dni` varchar(45) NOT NULL,
-  `email` varchar(75) NOT NULL,
-  `username` varchar(45) NOT NULL,
-  `enabled` bit(1) DEFAULT b'1',
-  `password` varchar(255) NOT NULL,
-  `role_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`,`role_id`),
-  KEY `fk_user_role_idx` (`role_id`),
-  CONSTRAINT `FKn82ha3ccdebhokx3a8fgdqeyy` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `tipo_documento` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `id` bigint NOT NULL,
+  `role_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKn82ha3ccdebhokx3a8fgdqeyy` (`role_id`),
+  CONSTRAINT `FKn82ha3ccdebhokx3a8fgdqeyy` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  CONSTRAINT `FKq9e1epiqip339syujm4brhw7u` FOREIGN KEY (`id`) REFERENCES `persona` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -77,7 +258,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Velez','Mati','DNI','12345678','mati@asd.com','mati',_binary '','$2a$10$A9IQzDnfuwlhTXyBLb2Y7e1KJBGY4QgpdCDwcAUWm.tC.yhO/uSZG',1),(3,'Ibañez','Diego','DNI','38079225','diego.hernan.ibanez@gmail.com','dieguito',_binary '','$2a$10$200DaOGdSfCyDROlnT0LIew0bD0Z27NymKzVntvqxWoN0FXRkiE0q',2),(8,'Sassenus','Milly','DNI','87654321','milly@asd.com','milly',_binary '','$2a$10$XyLWKTcwMoZpmO.T5rZCVO.VkMQsA5x.nnKIzYclEBFe2TiD8XzxK',1),(9,'Ortiz','Aylu','DNI','43215678','Aylu@asd.com','aylu',_binary '','$2a$10$QuxAnPSM3wZdGXgKV8fNDOldvWK8g8aEU8TMzFXoaQ8uEtpm92WbK',2),(11,'-','Inhabilitado','DNÏ','00000000','asd@asd.com','inhabilitado',_binary '\0','$2a$10$qJJL37mMJ88V1iV13u6QtuWpEtzlDV.7.LLXGlf37Sw5I0N3iDetO',1),(12,'-','Inhabilitado2','DNÏ','00000000','asd@asd.com','user',_binary '\0','$2a$10$EdODzvj5x9cMgY/RmFahKungT6LnnRiHyo8fJbbuFVR8jiQ2Z.F1W',2);
+INSERT INTO `user` VALUES ('aaa@aaa.com','$2a$10$lpeTNBUGJfHxsJNOkAF5IuS5eLZUOuL8vlHMd/9O0rL5Cb3rp.uk6','dni','mati',1,1);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -90,4 +271,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-25 21:17:47
+-- Dump completed on 2021-06-04 11:41:49
