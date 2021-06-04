785c1a3e8ad40012800aca04287e0a5a32ac5504
Milagros Sassenus <milu125000@gmail.com>
2021-06-04 00:09:20 -0300

    HAY TRAER POR FECHA CARAJO

--- src/main/java/com/example/aplication/WebSecurityConfig.java
+++ src/main/java/com/example/aplication/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         builder.jdbcAuthentication()
                 .dataSource(dataSource)
                 .passwordEncoder(passEncoder)
-                .usersByUsernameQuery("SELECT username, password, enabled FROM user u inner join persona p on u.id = p.id where username =?")
+                .usersByUsernameQuery("SELECT username, password, u.enabled FROM user u inner join persona p on u.id = p.id where username =?")
                 .authoritiesByUsernameQuery(
                         "Select u.username, r.tipo FROM role r inner join user u on r.id=u.role_id where u.username=?");
 
@@ -45,8 +45,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // "/js/**", "/images/**").permitAll().anyRequest().authenticated();  
         http
             .authorizeRequests()
-                .antMatchers(HttpMethod.GET,"/buscar/permiso").access("not(hasRole('ADMIN'))")
-                .antMatchers(HttpMethod.GET,"/buscar/permisoRodado").access("hasRole('AUDITOR')")
+                // .antMatchers(HttpMethod.GET,"/buscar/permiso").access("not(hasRole('ADMIN'))")
+                // .antMatchers(HttpMethod.GET,"/buscar/permisoRodado").access("hasRole('AUDITOR')")
 
                 .antMatchers("/", "/home", "/index", "/css/**", "/js/**", "/images/**","/permiso/**", "/rodados/**").permitAll().anyRequest()
         
--- src/main/java/com/example/aplication/controller/BuscarController.java
+++ src/main/java/com/example/aplication/controller/BuscarController.java
@@ -9,12 +9,17 @@ import com.example.aplication.service.PersonaServiceImplements;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -89,13 +94,49 @@ public class BuscarController {
     //     return "views/buscar/buscarRodado";
     // }
 
-    @RequestMapping("/permiso/fecha")
-    public String buscarPermisoFecha(Model model, @Param("dni") Long dni) {
-        List<Persona> listProducts = personaServiceImplements.listarDni(dni);
-        model.addAttribute("listProducts", listProducts);
-        model.addAttribute("dni", dni);
-
-        return "views/buscar/buscarFechaValida";
+    @ModelAttribute
+    LocalDate initLocalDate() {
+        return LocalDate.now();
     }
 
+    @RequestMapping(value = "/permiso/fecha", method = RequestMethod.GET)
+    public String viewsBuscarFecha(Model model) {
+        model.addAttribute("desde" , LocalDate.now().toString());
+        model.addAttribute("hasta", LocalDate.now().toString());
+        return "views/buscar/buscarfechaValida";
+    }
+
+    @RequestMapping(value = "/permiso/buscarFecha", method = RequestMethod.POST)
+    public String returnBuscarFecha(@ModelAttribute("hasta")String hasta, @ModelAttribute("desde")String desde,  Model model, RedirectAttributes attribute) {
+       
+        List<Permiso> lPermisos = permisoServiceImplements.listarTodos();
+        String[] hastas = hasta.split("-");
+        String[] desdes = desde.split("-");
+
+        LocalDate tope =  LocalDate.of( Integer.parseInt(hastas[0]), Integer.parseInt(hastas[1]), Integer.parseInt(hastas[2]));
+        LocalDate inicio =  LocalDate.of( Integer.parseInt(desdes[0]), Integer.parseInt(desdes[1]), Integer.parseInt(desdes[2]));
+        
+        if(tope.isBefore(inicio) || tope.equals(inicio)){
+            attribute.addFlashAttribute("error", "Ingrese las fechas correctas");
+            return "redirect:/buscar/permiso/fecha";
+        }
+       
+            for (int i = 0 ; i < lPermisos.size() ; i++)
+            {
+                if( lPermisos.get(i).getFecha().isAfter(tope)) lPermisos.remove(i);
+                if( lPermisos.get(i).getFecha().isBefore(inicio)) lPermisos.remove(i); 
+
+            }
+        
+        
+        if((lPermisos.get(0).getFecha().isAfter(tope) && (lPermisos.size() == 1))|| (lPermisos.get(0).getFecha().isBefore(inicio) && (lPermisos.size() == 1)))
+        {attribute.addFlashAttribute("warning", "No hay permisos en esas fechas");
+        return "redirect:/buscar/permiso/fecha";}
+
+        model.addAttribute("listProducts", lPermisos);
+        return "views/buscar/buscarfechaValida";
+    }
+
+
 }
+ 
