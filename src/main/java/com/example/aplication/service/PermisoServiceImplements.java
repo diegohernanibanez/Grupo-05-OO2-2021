d972473959467afa0353bb92c009c7f320d27519
Milagros Sassenus <milu125000@gmail.com>
2021-06-04 21:10:25 -0300

    funciona 100% con la logica de dieguito :D

--- src/main/java/com/example/aplication/service/PermisoServiceImplements.java
+++ src/main/java/com/example/aplication/service/PermisoServiceImplements.java
@@ -3,16 +3,11 @@ package com.example.aplication.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javassist.expr.Instanceof;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-import com.example.aplication.entity.Lugar;
 import com.example.aplication.entity.Permiso;
-import com.example.aplication.entity.PermisoDiario;
 import com.example.aplication.entity.PermisoPeriodo;
 import com.example.aplication.repository.PermisoRepository;
 
@@ -30,19 +25,20 @@ public class PermisoServiceImplements implements IPermisoService {
     @Override
     public List<Permiso> listarActivos(){
         List<Permiso> todos = listarTodos();
+        List<Permiso> rta = new ArrayList<>();
         for (int i = 0 ; i < todos.size() ; i++){
             Permiso p = todos.get(i);
                 if (p instanceof PermisoPeriodo){
-                   if (!(p.getFecha().plusDays(((PermisoPeriodo)p).getCantDias()).isBefore(LocalDate.now()))){
-                        todos.remove(i);    
+                   if ((p.getFecha().plusDays(((PermisoPeriodo)p).getCantDias()).isAfter(LocalDate.now()) || (p.getFecha().plusDays(((PermisoPeriodo)p).getCantDias()).isEqual(LocalDate.now())))){
+                         rta.add(p);    
                     }
                 }else{
-                    if (!(p.getFecha().plusDays(1).isBefore(LocalDate.now()))){
-                        todos.remove(i);
+                    if ((p.getFecha().plusDays(1).isAfter(LocalDate.now()) || p.getFecha().plusDays(1).isEqual(LocalDate.now()) )){
+                        rta.add(p);
                     }
                 }
         }
-        return todos;
+        return rta;
     }
 
     @Override
@@ -50,29 +46,15 @@ public class PermisoServiceImplements implements IPermisoService {
         List<Permiso> todos = listarActivos();
         List<Permiso> rta = new ArrayList<>(); 
 
-
         if(hasta.isBefore(desde)) throw new Exception("Desde mayor que hasta");
         for (int i = 0 ; i < todos.size() ; i++)
             {
                 Permiso p = todos.get(i);
-                if (p instanceof PermisoPeriodo){
-                   if ((p.getFecha().plusDays(((PermisoPeriodo)p).getCantDias()).isBefore(hasta) && p.getFecha().isAfter(desde)))
-                   {rta.add(p);
-                    
-                }
-                }else{
-                    if ((p.getFecha().plusDays(1).isBefore(hasta) && p.getFecha().isAfter(desde))){
-                        rta.add(p);
-                    }
-                }
-
+                   if (((p.getFecha().isBefore(hasta) || p.getFecha().isEqual(hasta) ) && (p.getFecha().isAfter(desde)||p.getFecha().isEqual(desde))))
+                   {rta.add(p);}
             }
-
         return rta;
     }
-
-  
-    
 
     @Override
     public Permiso buscarPorID(long id) {
